d44e9cec04a8d712c15267081e91d72e695cb900
Ganapatsa <ganapatsaratan@gmail.com>
2022-05-09 15:58:15 +0530

    second commit

---
+++ Dictionary.py
@@ -0,0 +1,22 @@
+#Datatype is Dictionary(key:value fair)and {} this are called as curly braces.
+#Note: It may be key or value it comes with any datatypes that is interger,float,string and where string comes quotes that is '' or "" should be used and called with quotes
+'''
+Dict = {1:"a","a":2.5,"b":"String"}
+
+print(Dict[1])
+print(Dict["b"])
+print(Dict["a"])
+'''
+
+#In Dictionary datatype entering key and value in empty dictionary{}.
+
+dic = {}
+
+dic["First name"] = 'Ganapatsa'
+dic["Last name"] = 'Ratan'
+
+#print(dic)
+print(dic["First name"])
+print(dic["Last name"])
+
+print(dic)
